75634bf6c99008f94adc0f4452596452fd459c1c
Geo Inca <geo.inca@hotmail.com>
2022-07-11 17:13:57 +0200

    id -> uuid

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
+            //$table->id();
+            $table->uuid('id')->primary();
             $table->string('uuid')->unique();
             $table->text('connection');
             $table->text('queue');
